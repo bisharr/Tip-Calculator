c077ac5bad44db5be025563c73bad2a7ce3fd666
Bishar Abdinur Mohamed <134773924+bisharr@users.noreply.github.com>
2023-11-02 07:32:33 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,25 +1,16 @@
 'use strict';
 
-// My Try
-// const calcBtn = document.getElementById('calculate');
-
-// calcBtn.addEventListener('click', function () {
-//   const totalamount =
-//     Number(document.getElementById('bill').value) +
-//     Number(document.getElementById('tip').value);
-//   document.getElementById('total').textContent = totalamount;
-// });
-
-const btnEl = document.getElementById('calculate');
-const billInput = document.getElementById('bill');
-const tipInput = document.getElementById('tip');
+const calcbtn = document.getElementById('calculate');
+const bill = document.getElementById('bill');
+const tip = document.getElementById('tip');
 const totalSpan = document.getElementById('total');
 
-const CalculateTotal = function () {
-  const billValue = billInput.value;
-  const tipValue = tipInput.value;
-  const totalValue = billValue * (1 + tipValue / 100);
+const calculateTotalAmount = function () {
+  const billVlue = bill.value;
+  const tipValue = tip.value;
+
+  const totalValue = billVlue * (1 + tipValue / 100);
   totalSpan.innerText = '$' + totalValue.toFixed(2);
 };
 
-btnEl.addEventListener('click', CalculateTotal);
+calcbtn.addEventListener('click', calculateTotalAmount);
